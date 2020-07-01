8c2b8dbcd22c1b44db6d561c43f17d998a5ed7ad
hackerman3102 <yagarwal3102@gmail.com>
2020-07-01 11:21:58 +0530

    added paragraphs and color

--- Text_to_handwriting/imagetry.py
+++ Text_to_handwriting/imagetry.py
@@ -4,45 +4,59 @@ from PIL import ImageFont
 import io
 import PIL
 import textwrap
+import new
+
+
+     
+
+
+
+
 
 
 f=open("solution.txt","r") #opens text file
-st=f.read()  #reads text file to a string
+st_list=f.readlines()  #reads text file to a list of string
 
 
-img = Image.new('RGB', (1366   , 768), (255,255,255)) #Creates white background image
-draw = ImageDraw.Draw(img) #creates object which can make edits to image img
-font=ImageFont.truetype("MyFont.ttf",size=30) #sets font and fontsize
 
-a=30  #start making line from y coordinate=30
-while(a<768):  #make lines upto y coodinate 768
+font=ImageFont.truetype("MyFont.ttf",size=30)
+pagenum=1
+img,draw=new.newImg()
+y_text=100
+newpage=False
+control=0
+height=30
 
-    draw.line((0,a,1366,a), fill=(0,0,0)) #make line from (0,a) to (1366,a).Note general res is 1466*768
-    a+=30  # increase a by value of height found in line 40
+print(st_list)
 
-
-lines=textwrap.wrap(st,width=90) #convert the string to lines based on widtg(no of characters)
-y_text=0 #initial height
-for line in lines:  
-    width, height = font.getsize(line) #get height of line used for creating lines
-    lst=line.split()
-    nline="  ".join(lst) #above two lines is to create more spacing betwween words due to the issue being faced
-    if("?" in line):#if line contains ?
-        draw.text((0, y_text), nline, font=font, fill=(0,0,255),spacing=10)#print line in blue
-
-
+for para in st_list:
+    if("?" in para):
+        color=(0,0,255)
     else:
+        color=(0,0,0)    
 
-        draw.text((0, y_text), nline+" ", font=font, fill=(0,0,0),spacing=10)#print line in blakc
-
+    textlines=textwrap.wrap(para,width=55)
+    for line in textlines:
+        if(y_text>768):
+            newpage=True  
+        if(newpage):
+            img.save("image{}.jpg".format(pagenum)) 
+            pagenum+=1
+            img,draw=new.newImg()
+            y_text=100
+            newpage=False
+            
         
-    y_text += height#next line to start at current height + height
-    print(height)
+        
+        lst=line.split()
+        nline="   ".join(lst)
+        draw.text((100, y_text), nline, font=font, fill=color,spacing=10)
+        y_text += height
+    y_text+=height    
+        
+img.save("image{}.jpg".format(pagenum)) 
+img.show()              
 
 
-pagenum=1    #trick to create multiple photos
-img.save("image{}.jpg".format(pagenum))    #save image in folder as dynamic name
 
 
-
-img.show() #display image
--- Text_to_handwriting/new.py
+++ Text_to_handwriting/new.py
@@ -17,34 +17,6 @@ def newImg():
         draw.line((0,a+100,1366,a+100), fill=(0,0,0)) #make line from (0,a) to (1366,a).Note general res is 1466*768
         a+=30  # increase a by value of height found in line 40
     
-    return img
-def ImageToPdf():
-    imageto=Image.open(r'C:\Users\Sushmita Agrawal\Downloads\TextToHandwriting\text_to_handwriting\Text_to_handwriting\image1.jpg')
-    im1 = imageto.convert('RGB')
-    im1.save(r'C:\Users\Sushmita Agrawal\Downloads\TextToHandwriting\text_to_handwriting\Text_to_handwriting\im1.pdf')
-
-f=open("solution.txt","r") #opens text file
-st=f.read()  #reads text file to a string
+    return img,draw
 
 
-img=newImg() #func call
-
-font=ImageFont.truetype("MyFont.ttf",size=30)
-draw = ImageDraw.Draw(img)
-lines=textwrap.wrap(st,width=65)
-y_text=100
-for line in lines:  
-    width, height = font.getsize(line) #get height of line used for creating lines
-    lst=line.split()
-    nline="   ".join(lst) #above two lines is to create more spacing betwween words due to the issue being faced
-    if("?" in line):#if line contains ?
-        draw.text((100, y_text), nline, font=font, fill=(0,0,255),spacing=10)#print line in blue
-    else:
-        draw.text((100, y_text), nline+" ", font=font, fill=(0,0,0),spacing=10)#print line in blakc    
-    y_text += height#next line to start at current height + height
-
-img.show() 
-pagenum=1    #trick to create multiple photos
-img.save("image{}.jpg".format(pagenum))    #save image in folder as dynamic name
-
-ImageToPdf()
